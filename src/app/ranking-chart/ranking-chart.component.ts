821650d8aebdcae8bb3c186f627988640b9518c6
MaartenGOrdina <maarten.grootoonk@ordina.nl>
2024-11-21 14:10:44 +0100

    Add countdown for ranking

--- src/app/ranking-chart/ranking-chart.component.ts
+++ src/app/ranking-chart/ranking-chart.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {Component, OnDestroy} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-ranking-chart',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgClass,
+    NgIf
   ],
   templateUrl: './ranking-chart.component.html',
   styleUrl: './ranking-chart.component.scss'
 })
-export class RankingChartComponent {
+export class RankingChartComponent implements OnDestroy {
   rankedUsers = [
     {progress: 70, points: 70, name: 'Maarten'},
     {progress: 65, points: 65, name: 'Jan'},
@@ -18,4 +20,50 @@ export class RankingChartComponent {
     {progress: 45, points: 45, name: 'Gerbert'},
     {progress: 43, points: 43, name: 'Johan'},
   ];
+
+  public hasStarted = false;
+  public intervalRef: any = null;
+
+  public formattedCountdown = '';
+
+  constructor() {
+    this.formattedCountdown = this.getFormattedTimeLeftTillStart();
+    this.setupCountdown();
+  }
+
+
+  setupCountdown(){
+    this.intervalRef = setInterval(() => {
+      this.formattedCountdown = this.getFormattedTimeLeftTillStart();
+    }, 1000);
+  }
+
+  getFormattedTimeLeftTillStart(){
+    const startDate = new Date(2024, 11, 1, 6, 0, 0);
+    const total = startDate.getTime() - new Date().getTime();
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+
+    let formattedCountdown = '';
+
+    if(days > 0){
+      formattedCountdown += days + " days, ";
+    }
+
+    if(hours > 0){
+      formattedCountdown += hours + " hours, and ";
+    }
+
+    if(minutes > 0){
+      formattedCountdown += minutes + " minutes";
+    }
+
+    return formattedCountdown
+  }
+
+  ngOnDestroy(){
+    clearTimeout(this.intervalRef);
+  }
+
 }
